838d2e0e0eba4ddcc345966210d2db7fd448874b
Manu <manueljesusrejonsantana@gmail.com>
2024-12-19 23:14:50 -0600

    chore: color and welcome added

---
+++ src/admin/app.ts
@@ -0,0 +1,62 @@
+export default {
+    config: {
+        translations: {
+            en: {
+                "app.components.LeftMenu.navbrand.title": "Sitio de prueba",
+                "Auth.form.welcome.title": "Bienvenido a tu Sitio de prueba",
+            },
+        },
+        theme: {
+            // Se recomienda modificar únicamente los colores marcados con el comentario "Color modificable" para asegurar una experiencia de usuario óptima.
+            dark: {
+                colors: {
+                    alternative100: "#181f26", // Color modificable
+                    alternative200: "#4a596a", // Color modificable
+                    alternative500: "#73a9e6", // Color modificable
+                    alternative600: "#73a9e6", // Color modificable
+                    alternative700: "#c1daf4", // Color modificable
+                    buttonNeutral0: "#212a34", // color para el texto de los botones
+                    buttonPrimary500: "#79b8ff", // Color modificable
+                    buttonPrimary600: "#45a2ff", // Color modificable
+                    danger100: "#181f26", // Color modificable
+                    danger200: "#4a596a", // Color modificable
+                    danger500: "#ee5e52",
+                    danger600: "#ee5e52",
+                    danger700: "#ee5e52",
+                    neutral0: "#212a34", // COLOR DE FONDO PRINCIPAL!!!
+                    neutral100: "#181f26", // Color modificable
+                    neutral1000: "#ffffff",
+                    neutral150: "#323f4d", // Color modificable
+                    neutral200: "#4a596a", // Color modificable
+                    neutral300: "#667687", // Color modificable
+                    neutral400: "#a5afba", // Color modificable
+                    neutral500: "#c0c8cf", // Color modificable
+                    neutral600: "#a5afba", // Color modificable
+                    neutral700: "#eaecef", // Color modificable
+                    neutral800: "#ffffff",
+                    neutral900: "#ffffff",
+                    primary100: "#181f26", // Color modificable
+                    primary200: "#4a596a", // Color modificable
+                    primary500: "#45a2ff", // Color modificable
+                    primary600: "#79b8ff", // Color modificable
+                    primary700: "#79b8ff", // Color modificable
+                    secondary100: "#181f26", // Color modificable
+                    secondary200: "#4a596a", // Color modificable
+                    secondary500: "#66b7f1",
+                    secondary600: "#66b7f1",
+                    secondary700: "#b8e1ff",
+                    success100: "#181f26", // Color modificable
+                    success200: "#4a596a", // Color modificable
+                    success500: "#5cb176",
+                    success600: "#5cb176",
+                    success700: "#c6f0c2",
+                    warning100: "#181f26", // Color modificable
+                    warning200: "#4a596a", // Color modificable
+                    warning500: "#f29d41",
+                    warning600: "#f29d41",
+                    warning700: "#fae7b9",
+                },
+            },
+        },
+    },
+};
